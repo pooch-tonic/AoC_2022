3f74160494bab8a6c60889f8a79dc07d90deb79e
Ryo SHIINA <ryo.shiina@viacesi.fr>
2022-12-26 22:36:05 +0100

    day 25 part 1 done

--- day_25/main.js
+++ day_25/main.js
@@ -3,6 +3,8 @@ const { input, testInput } = require("./input");
 
 const base = 5;
 
+const conversionDict = ["0", "1", "2", "=", "-"];
+
 const convertToDecimal = (snafuNumber) => {
   let decimal = 0;
   snafuNumber.split("").forEach((e, index) => {
@@ -29,12 +31,27 @@ const convertToDecimal = (snafuNumber) => {
   return decimal;
 };
 
-sumAll = (input) => {
+const convertToSnafu = (decimalNumber) => {
+  let res = "";
+  let power = 0;
+  while (convertToDecimal(res) !== decimalNumber) {
+    const dictIndex =
+      Math.round(
+        (parseInt(decimalNumber) % Math.pow(base, power + 1)) /
+          Math.pow(base, power)
+      ) % base;
+    res = conversionDict[dictIndex] + res;
+    power++;
+  }
+  return res;
+};
+
+const sumAll = (input) => {
   let sum = 0;
   return input.split("\n").reduce((acc, val) => acc + convertToDecimal(val), 0);
 };
 
-console.log(sumAll(input));
+console.log(convertToSnafu(sumAll(input)));
 
 // use this line for in-depth debugging
 // console.log(util.inspect(myObject, {showHidden: false, depth: null, colors: true}))
